cd3e2bad9f758a21a0b858270448f1e4296b3b81
Grzegorz Paśnik <grzesiekmos@gmail.com>
2016-09-20 14:08:05 +0200

    added console nlog target

--- src/Coolector.Api/Modules/HomeModule.cs
+++ src/Coolector.Api/Modules/HomeModule.cs
@@ -1,14 +1,25 @@
-﻿using Nancy;
+﻿using System.Threading.Tasks;
+using Nancy;
+using NLog;
 
 namespace Collector.Api.Modules
 {
     public class HomeModule : NancyModule
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public HomeModule()
         {
             Get("/", args => "Hello from Nancy running on CoreCLR");
 
-            Get("/test/{name}", args => $"Test parameter: {args.name}");
+            Get("/test/{name}", TestAsync);
+        }
+
+        private async Task<object> TestAsync(dynamic args)
+        {
+            Logger.Info($"TestAsync args: {args.name}");
+
+            return $"Test parameter: {args.name}";
         }
     }
 }
