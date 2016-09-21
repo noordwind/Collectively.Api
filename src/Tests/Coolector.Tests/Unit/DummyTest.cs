e90385a7ac926ff877139fe92bb51e21c41a9add
Piotr Gankiewicz <spetz90@gmail.com>
2016-09-21 09:46:40 +0200

    Test projects with NUnit #5

---
+++ src/Tests/Coolector.Tests/Unit/DummyTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Coolector.Tests.Unit
+{
+    [TestFixture]
+    public class DummyTest
+    {
+        [Test]
+        public void TrueShouldBeTrue()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
