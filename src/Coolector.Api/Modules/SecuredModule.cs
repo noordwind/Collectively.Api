1e44f2b5d3f02fbcf0899e64602b68efe866f4b7
Grzegorz Paśnik <grzesiekmos@gmail.com>
2016-09-21 15:17:35 +0200

    integration with auth0 authentication added, resolved #9

---
+++ src/Coolector.Api/Modules/SecuredModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using Nancy.Security;
+
+namespace Coolector.Api.Modules
+{
+    public class SecuredModule : NancyModule
+    {
+        public SecuredModule()
+        {
+            this.RequiresAuthentication();
+
+            Get("/secured/{name}", args => $"Args: {args.name}, user:{Context.CurrentUser.Identity.Name}");
+        }
+    }
+}
--- src/Coolector.Api/Startup.cs
+++ src/Coolector.Api/Startup.cs
@@ -1,8 +1,14 @@
-﻿using Coolector.Api.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Coolector.Api.Framework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Nancy.Owin;
 using NLog.Extensions.Logging;
 
@@ -11,6 +17,7 @@ namespace Coolector.Api
     public class Startup
     {
         public IConfiguration Configuration { get; set; }
+        public IContainer ApplicationContainer { get; set; }
 
         public Startup(IHostingEnvironment env)
         {
@@ -23,11 +30,38 @@ namespace Coolector.Api
             Configuration = builder.Build();
         }
 
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddWebEncoders();
+            ApplicationContainer = GetServiceContainer(services);
+
+            return new AutofacServiceProvider(ApplicationContainer);
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddNLog();
             env.ConfigureNLog("nlog.config");
+            var options = new JwtBearerOptions
+            {
+                Audience = Configuration["auth0:clientId"],
+                Authority = $"https://{Configuration["auth0:domain"]}/",
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+                }
+            };
+            app.UseJwtBearerAuthentication(options);
             app.UseOwin().UseNancy(x => x.Bootstrapper = new CoolectorBootstrapper(Configuration));
+            
+        }
+
+        protected static IContainer GetServiceContainer(IEnumerable<ServiceDescriptor> services)
+        {
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            return builder.Build();
         }
     }
 }
