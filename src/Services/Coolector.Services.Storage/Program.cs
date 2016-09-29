d87dd532d255c8ff96a56d0825bbb92142d88876
Grzegorz Paśnik <grzesiekmos@gmail.com>
2016-09-29 10:23:24 +0200

    storage service initial commit #16

---
+++ src/Services/Coolector.Services.Storage/Program.cs
@@ -0,0 +1,18 @@
+﻿using Coolector.Services.Host;
+using Coolector.Services.Storage.Framework;
+
+namespace Coolector.Services.Storage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebServiceHost
+                .Create<Startup>(port: 10000)
+                .UseAutofac(Bootstrapper.LifeTimeScope)
+                .UseRabbitMq()
+                .Build()
+                .Run();
+        }
+    }
+}
