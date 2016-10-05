73b5998d26d9442c8b446b4bdc30217e78c4f7fa
Grzegorz Paśnik <grzesiekmos@gmail.com>
2016-10-05 14:47:08 +0200

    provider client unit tests, #26

---
+++ src/Tests/Coolector.Tests/Framework/IStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Coolector.Common.Types;
+
+namespace Coolector.Tests.Framework
+{
+    public interface IStorage
+    {
+        Task<Maybe<object>> FetchAsync();
+        Task SaveAsync(object obj);
+    }
+}
