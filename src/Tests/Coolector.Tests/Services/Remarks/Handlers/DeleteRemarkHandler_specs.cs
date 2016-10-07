2c047963f29a951c7f7415d6daff98e2b345c7da
Grzegorz Paśnik <grzesiekmos@gmail.com>
2016-10-07 13:02:25 +0200

    delete remark handler unit tests #30

---
+++ src/Tests/Coolector.Tests/Services/Remarks/Handlers/DeleteRemarkHandler_specs.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Coolector.Common.Commands.Remarks;
+using Coolector.Common.Events.Remarks;
+using Coolector.Services.Domain;
+using Coolector.Services.Remarks.Handlers;
+using Coolector.Services.Remarks.Services;
+using Machine.Specifications;
+using Moq;
+using RawRabbit;
+using RawRabbit.Configuration.Publish;
+using It = Machine.Specifications.It;
+
+namespace Coolector.Tests.Services.Remarks.Handlers
+{
+    public class DeleteRemarkHandler_specs
+    {
+        protected static DeleteRemarkHandler Handler;
+        protected static Mock<IBusClient> BusClientMock;
+        protected static Mock<IRemarkService> RemarkServiceMock;
+
+        protected static DeleteRemark Command;
+        protected static Exception Exception;
+
+        protected static void Initialize()
+        {
+            BusClientMock = new Mock<IBusClient>();
+            RemarkServiceMock = new Mock<IRemarkService>();
+
+            Command = new DeleteRemark
+            {
+                UserId = "userId",
+                RemarkId = Guid.NewGuid()
+            };
+
+            Handler = new DeleteRemarkHandler(BusClientMock.Object, RemarkServiceMock.Object);
+        }
+    }
+
+    [Subject("DeleteRemarkHandler HandleAsync")]
+    public class when_invoking_delete_remark_handle_async : DeleteRemarkHandler_specs
+    {
+        Establish context = () =>
+        {
+            Initialize();
+        };
+
+        Because of = () => Handler.HandleAsync(Command).Await();
+
+        It should_call_delete_async_on_remark_service = () =>
+        {
+            RemarkServiceMock.Verify(x => x.DeleteAsync(Moq.It.IsAny<Guid>(), Moq.It.IsAny<string>()), Times.Once);
+        };
+
+        It should_publish_remark_deleted_event = () =>
+        {
+            BusClientMock.Verify(x => x.PublishAsync(Moq.It.IsAny<RemarkDeleted>(), 
+                Moq.It.IsAny<Guid>(), 
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()), Times.Once);
+        };
+    }
+
+
+    [Subject("DeleteRemarkHandler HandleAsync")]
+    public class when_invoking_delete_remark_handle_async_and_service_throws_exception : DeleteRemarkHandler_specs
+    {
+        Establish context = () =>
+        {
+            Initialize();
+            RemarkServiceMock.Setup(x => x.DeleteAsync(Moq.It.IsAny<Guid>(), Moq.It.IsAny<string>()))
+                .Throws<ServiceException>();
+        };
+
+        Because of = () => Exception = Catch.Exception(() => Handler.HandleAsync(Command).Await());
+
+        It should_call_delete_async_on_remark_service = () =>
+        {
+            RemarkServiceMock.Verify(x => x.DeleteAsync(Moq.It.IsAny<Guid>(), Moq.It.IsAny<string>()), Times.Once);
+        };
+
+        It should_not_publish_remark_deleted_event = () =>
+        {
+            BusClientMock.Verify(x => x.PublishAsync(Moq.It.IsAny<RemarkDeleted>(),
+                Moq.It.IsAny<Guid>(),
+                Moq.It.IsAny<Action<IPublishConfigurationBuilder>>()), Times.Never);
+        };
+    }
+}
