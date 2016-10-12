278fb29bb8413a8b3c13235767b58da48ebc52eb
Piotr Gankiewicz <spetz90@gmail.com>
2016-10-12 07:58:00 +0200

    E2E tests auth fix #32

--- src/Tests/Coolector.Tests.EndToEnd/API/ModuleBase_specs.cs
+++ src/Tests/Coolector.Tests.EndToEnd/API/ModuleBase_specs.cs
@@ -1,10 +1,10 @@
-﻿using System.Threading.Tasks;
-using Coolector.Tests.EndToEnd.Framework;
+﻿using Coolector.Tests.EndToEnd.Framework;
 
 namespace Coolector.Tests.EndToEnd.API
 {
     public abstract class ModuleBase_specs
     {
+        private static bool _authenticated;
         protected static Auth0SignInResponse Auth0SignInResponse;
         protected static IHttpClient HttpClient = new CustomHttpClient("http://localhost:5000");
 
@@ -17,16 +17,20 @@ namespace Coolector.Tests.EndToEnd.API
                 Authenticate();
         }
 
-        protected static Task<Auth0SignInResponse> GetAuth0SignInResponseAsync()
-            => Auth0Client.SignInAsync("noordwind-test1@mailinator.com", "test");
-
         protected static void SignInToAuth0()
-            => Auth0SignInResponse = GetAuth0SignInResponseAsync().WaitForResult();
+            => Auth0SignInResponse = GetAuth0SignInResponse();
+
+        protected static Auth0SignInResponse GetAuth0SignInResponse()
+            => Auth0Client.SignInAsync("noordwind-test1@mailinator.com", "test").WaitForResult();
 
         protected static void Authenticate()
         {
+            if (_authenticated)
+                return;
+
             SignInToAuth0();
             HttpClient.SetHeader("Authorization", $"Bearer {Auth0SignInResponse.IdToken}");
+            _authenticated = true;
         }
     }
 }
