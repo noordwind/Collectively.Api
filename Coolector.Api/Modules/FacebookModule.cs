0a1d0c05254533224e9fac45ce4b22afaf910ed3
spetz <spetz90@gmail.com>
2016-11-18 12:44:03 +0100

    Facebook module

---
+++ Coolector.Api/Modules/FacebookModule.cs
@@ -0,0 +1,18 @@
+﻿using Coolector.Api.Commands;
+using Coolector.Api.Validation;
+using Coolector.Common.Commands.Facebook;
+
+namespace Coolector.Api.Modules
+{
+    public class FacebookModule : ModuleBase
+    {
+        public FacebookModule(ICommandDispatcher commandDispatcher,
+            IValidatorResolver validatorResolver)
+            : base(commandDispatcher, validatorResolver, "social/facebook")
+        {
+            Post("wall", async args => await For<PostMessageOnFacebookWall>()
+                .OnSuccessAccepted("account")
+                .DispatchAsync());
+        }
+    }
+}
