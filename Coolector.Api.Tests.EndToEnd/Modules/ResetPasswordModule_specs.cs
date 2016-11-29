6d81319575a865c21b0dcb9ea6a144e8a4674602
spetz <spetz90@gmail.com>
2016-11-29 12:29:56 +0100

    Reset password E2E tests

---
+++ Coolector.Api.Tests.EndToEnd/Modules/ResetPasswordModule_specs.cs
@@ -0,0 +1,36 @@
+﻿using Coolector.Api.Tests.EndToEnd.Framework;
+using Coolector.Common.Dto.Operations;
+using Machine.Specifications;
+
+namespace Coolector.Api.Tests.EndToEnd.Modules
+{
+    public abstract class ResetPasswordModule_specs : ModuleBase_specs
+    {
+        protected static OperationDto InitiateResetPassword()
+        {
+            return OperationHandler.PostAsync("reset-password", new
+            {
+                email = TestEmail
+            }).WaitForResult();
+        }
+    }
+
+    [Subject("Initiate reset password")]
+    public class when_initiating_password_reset : ResetPasswordModule_specs
+    {
+        protected static OperationDto Result;
+
+        Establish context = () =>
+        {
+            Initialize();
+            Wait();
+        };
+
+        Because of = () => Result = InitiateResetPassword();
+
+        It should_return_success_status_code = () =>
+        {
+            Result.Success.ShouldBeTrue();
+        };
+    }
+}
