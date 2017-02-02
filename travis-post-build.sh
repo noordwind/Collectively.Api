f3df9b40ee303e1b5f1b9b2eddd582c7a4084cd5
spetz <spetz90@gmail.com>
2017-02-02 11:02:52 +0100

    Travis build echo

--- travis-post-build.sh
+++ travis-post-build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo Triggering Docker Hub registry using branch $TRAVIS_BRANCH
 curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "$TRAVIS_BRANCH"}' -X POST $DOCKER_HUB_TRIGGER_URL
