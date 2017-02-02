6c230fe5cb85880fa88f1fa0efae0f30cf2b1a12
spetz <spetz90@gmail.com>
2017-02-02 11:24:56 +0100

    Travis CI final script (for now)

--- travis-post-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo Triggering Docker Hub registry using branch $TRAVIS_BRANCH and webhook URL $DOCKER_HUB_TRIGGER_URL
-curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "$TRAVIS_BRANCH"}' -X POST $DOCKER_HUB_TRIGGER_URL
