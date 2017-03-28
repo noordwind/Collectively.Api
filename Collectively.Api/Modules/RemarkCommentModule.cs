9d5eab77f588a256cd7990c6ca873fe05d822a70
spetz <spetz90@gmail.com>
2017-03-28 10:57:59 +0200

    Remark comments module

---
+++ Collectively.Api/Modules/RemarkCommentModule.cs
@@ -0,0 +1,28 @@
+﻿using Collectively.Api.Commands;
+using Collectively.Api.Storages;
+using Collectively.Api.Validation;
+using Collectively.Messages.Commands.Remarks;
+
+namespace Collectively.Api.Modules
+{
+  public class RemarkCommentModule : ModuleBase
+    {
+        public RemarkCommentModule(ICommandDispatcher commandDispatcher,
+            IRemarkStorage remarkStorage,
+            IValidatorResolver validatorResolver)
+            : base(commandDispatcher, validatorResolver, modulePath: "remarks/{remarkId}/comments")
+        {
+            Post("", async args => await For<AddCommentToRemark>()
+                .OnSuccessAccepted()
+                .DispatchAsync());
+
+            Put("{commentId}", async args => await For<EditRemarkComment>()
+                .OnSuccessAccepted()
+                .DispatchAsync());
+
+            Delete("{commentId}", async args => await For<DeleteRemarkComment>()
+                .OnSuccessAccepted()
+                .DispatchAsync());
+        }
+    }
+}
