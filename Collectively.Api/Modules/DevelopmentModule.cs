a5273ab43889cc71f223b994def7f3ab638ca2c0
Piotr Gankiewicz <spetz90@gmail.com>
2017-04-12 21:28:11 +0200

    Dev module

---
+++ Collectively.Api/Modules/DevelopmentModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Collectively.Api.Commands;
+using Collectively.Api.Storages;
+using Collectively.Api.Validation;
+using Collectively.Services.Storage.Models.Operations;
+
+namespace Collectively.Api.Modules
+{
+    public class DevelopmentModule : ModuleBase
+    {
+        public DevelopmentModule(ICommandDispatcher commandDispatcher,
+            IValidatorResolver validatorResolver,
+            IOperationStorage operationStorage)
+            : base(commandDispatcher, validatorResolver, modulePath: "development")
+        {
+            Get("operation/{state}", args => 
+            {
+                return new Operation
+                {
+                    Id = Guid.NewGuid(),
+                    RequestId = Guid.NewGuid(),
+                    CreatedAt = DateTime.UtcNow,
+                    State = args.state
+                };
+            });
+        }
+    }
+}
