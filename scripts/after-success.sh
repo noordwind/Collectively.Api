56f5d0d808b196912557d9a4bcc0affc9b9dc5bf
Piotr Gankiewicz <spetz90@gmail.com>
2017-07-14 00:06:15 +0200

    Updated after success script

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo Building and pushing Docker images on branch $TRAVIS_BRANCH
+echo Executing after success scripts on branch $TRAVIS_BRANCH
+echo Publishing application
+./scripts/dotnet-publish.sh
+echo Building and pushing Docker images
 ./scripts/docker-publish-ci.sh
