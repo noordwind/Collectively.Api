f9ce71330e299ff4b6d9abc6f18e14a6d791d10a
Piotr Gankiewicz <spetz90@gmail.com>
2017-07-14 00:01:54 +0200

    dotnet publish

--- scripts/docker-publish-ci.sh
+++ scripts/docker-publish-ci.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 DOCKER_ENV=''
+DOCKER_TAG=''
 case "$TRAVIS_BRANCH" in
   "master")
     DOCKER_ENV=production
+    DOCKET_TAG=''
     ;;
   "develop")
     DOCKER_ENV=development
+    DOCKER_TAG=dev
     ;;    
 esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -f ./src/Collectively.Api/Dockerfile.$DOCKER_ENV -t collectively.api:$DOCKER_ENV ./src/Collectively.Api
-docker tag collectively.api:$DOCKER_ENV $DOCKER_USERNAME/collectively.api:$DOCKER_ENV
-docker push $DOCKER_USERNAME/collectively.api:$DOCKER_ENV
+docker build -f ./src/Collectively.Api/Dockerfile.$DOCKER_ENV -t collectively.api:$DOCKER_TAG ./src/Collectively.Api
+docker tag collectively.api:$DOCKER_TAG $DOCKER_USERNAME/collectively.api:$DOCKER_TAG
+docker push $DOCKER_USERNAME/collectively.api:$DOCKER_TAG
---
+++ scripts/dotnet-publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+dotnet publish ./src/Collectively.Api -c Release -o ./bin/Docker
