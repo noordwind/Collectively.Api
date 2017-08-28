a44ddae4888f722d911ef76e993ade60886646d6
Piotr Gankiewicz <spetz90@gmail.com>
2017-08-28 12:13:55 +0200

    Fixed IoC for operation provider

--- src/Collectively.Api/Modules/OperationModule.cs
+++ src/Collectively.Api/Modules/OperationModule.cs
@@ -6,6 +6,8 @@ using Collectively.Common.Types;
 using Collectively.Common.Extensions;
 using Collectively.Services.Storage.Models.Operations;
 using Nancy.Security;
+using Collectively.Api.Services;
+using Nancy;
 
 namespace Collectively.Api.Modules
 {
@@ -13,20 +15,24 @@ namespace Collectively.Api.Modules
     {
         public OperationModule(ICommandDispatcher commandDispatcher,
             IValidatorResolver validatorResolver,
-            IOperationStorage operationStorage)
+            IOperationProvider operationProvider)
             : base(commandDispatcher, validatorResolver, modulePath: "operations")
         {
             Get("{requestId}", args => Fetch<GetOperation, Operation>
             (async x =>
             {
-                var operation = await operationStorage.GetAsync(x.RequestId);
-                if (operation.HasNoValue || operation.Value.UserId.Empty())
+                var operation = await operationProvider.GetAsync(x.RequestId);
+                if (operation == null)
+                {
+                    return new Maybe<Operation>();
+                }
+                if (operation.UserId.Empty())
                 {
                     return operation;
                 }
                 this.RequiresAuthentication();
 
-                return operation.Value.UserId == CurrentUserId
+                return operation.UserId == CurrentUserId
                     ? operation
                     : new Maybe<Operation>();
             }).HandleAsync());
