d4ba5130f8560970583eb5c4a7d1f16f859765d5
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-06 15:11:37 +0200

    Added nuget source

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -6,7 +6,7 @@ case "$TRAVIS_BRANCH" in
     ;;
 esac
 
-dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively$MYGET_ENV/api/v3/index.json" --no-cache
+dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively$MYGET_ENV/api/v3/index.json" --source "https://www.myget.org/F/imagesharp/api/v3/index.json" --no-cache
 dotnet build --no-restore
 
 
